bb2361dc4ae3ebfe99c7b67b621f906463bc1059
dacharat <dacharat.p@ku.th>
2019-04-22 12:00:29 +0700

    :iphone: add JoinEvents page

--- components/user/JoinEvents.js
+++ components/user/JoinEvents.js
@@ -1,12 +1,42 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
 
-const JoinEvents = () => {
+const JoinEvents = ({ auth }) => {
   return (
     <ScrollView>
-      <Text>JoinEvents</Text>
+      {auth.join.map((j, i) => (
+        <View key={i} style={styles.eventContent}>
+          <Text style={styles.eventTitle}>{j}</Text>
+        </View>
+      ))}
     </ScrollView>
   );
 };
 
-export default JoinEvents;
+const styles = StyleSheet.create({
+  eventContent: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    padding: 30,
+    flex: 2,
+    borderWidth: 0.5
+  },
+  eventTitle: {
+    height: "100%",
+    fontSize: 25
+  },
+  button: {
+    width: 30,
+    height: 30
+  },
+  buttonText: {
+    fontSize: 25
+  }
+});
+
+const mapStateToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps)(JoinEvents);
